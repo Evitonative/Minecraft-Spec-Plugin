58e143b67ad28a8d1f098f5ea9726ed9d0cd1283
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-12-01 20:49:16 +0100

    new start

---
+++ src/main/java/me/evito/survivalspectator/survivalspectator/SurvivalSpectator.java
@@ -0,0 +1,17 @@
+package me.evito.survivalspectator.survivalspectator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalSpectator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
